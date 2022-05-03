0a2734a05104bc055856a8f2dbe457b55f4e5055
IvanMatt8 <ivanmatt1995@gmail.com>
2022-05-03 10:57:17 -0300

    build: wrapper widgetForm

--- src/components/widgetForm/WidgetForm.tsx
+++ src/components/widgetForm/WidgetForm.tsx
@@ -2,6 +2,21 @@ export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg  w-[calc(100vw-2rem)] md:w-auto">
       Hellow word
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+      </header>
+      <p>Hello World</p>
+      <footer>
+        <span className="text-xs text-neutral-400 ">
+          Feito com ♥
+          <a
+            className="underline underline-offset-2 p-1"
+            href="https://www.instagram.com/ivanmatt8/"
+          >
+            IvanMattDev
+          </a>
+        </span>
+      </footer>
     </div>
   )
 }
