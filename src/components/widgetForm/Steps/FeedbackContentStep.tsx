9fdca2c48e911890d5cadc14356d5f8c0a9f3734
IvanMatt8 <ivanmatt1995@gmail.com>
2022-05-04 12:35:13 -0300

    feat: separating functionality

---
+++ src/components/widgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,38 @@
+import { FeedbackType, feedbackTypes } from '..'
+import { ArrowLeft } from 'phosphor-react'
+import { CloseButton } from '../../closeButton/closeButton'
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType
+}
+
+export function FeedbackContentStep({
+  feedbackType,
+}: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          className="top-5 left-5 absolute text-zinc-500 hover:text-zinc-100"
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2 ">
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
+          {feedbackTypeInfo.title}
+        </span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full"></div>
+    </>
+  )
+}
