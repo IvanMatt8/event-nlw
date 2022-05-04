91b72d71323f4f9cd0286bab1fd83ed2180550f3
IvanMatt8 <ivanmatt1995@gmail.com>
2022-05-04 12:32:32 -0300

    refactor: adding flex in widget

--- src/components/widget/Widget.tsx
+++ src/components/widget/Widget.tsx
@@ -1,10 +1,10 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
-import { WidgetForm } from '../widgetForm/WidgetForm'
+import { WidgetForm } from '../widgetForm/index'
 
 export function Widget() {
   return (
-    <Popover className="absolute bottom-4 md:bottom-8 md:right-8 right-4 flex-col items-end">
+    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
       <Popover.Panel>
         <WidgetForm />
       </Popover.Panel>
@@ -12,7 +12,7 @@ export function Widget() {
         <ChatTeardropDots className="w-6 h-6" />
 
         <span className="max-w-0  overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-          <span className="max-w-0"></span>
+          <span className="pl-2"></span>
           FeedBack
         </span>
       </Popover.Button>
