e94e7b6b941250cbe2fba8ba9d6956ee75ee88be
IvanMatt8 <ivanmatt1995@gmail.com>
2022-05-04 12:35:55 -0300

    refactor: import closeButton

---
+++ src/components/widgetForm/index.tsx
@@ -0,0 +1,59 @@
+import { CloseButton } from '../closeButton/closeButton'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import otherImageUrl from '../../assets/other.svg'
+import { useState } from 'react'
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
+import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lampada',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: otherImageUrl,
+      alt: 'Imagem de uma nuvem de pensamentos',
+    },
+  },
+}
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg  w-[calc(100vw-2rem)] md:w-auto">
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep feedbackType={feedbackType} />
+      )}
+
+      <footer>
+        <span className="text-xs text-neutral-400 ">
+          Feito com ♥
+          <a
+            className="underline underline-offset-2 p-1"
+            href="https://www.instagram.com/ivanmatt8/"
+          >
+            IvanMattDev
+          </a>
+        </span>
+      </footer>
+    </div>
+  )
+}
